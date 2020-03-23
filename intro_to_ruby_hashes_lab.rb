1790500ea1af4c2bbc479ed73d1c2dd084d1a647
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-23 14:53:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,13 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator = {
+    :nationality => "italian"
+
+  }
+  # return a hash that includes the key and value parameters passed into this method. 
+  #In this method, create and return a hash with one key/value pair using the first paramter as the key
+  #and the second paramater as the value
 end
 
 def read_from_hash(hash, key)
