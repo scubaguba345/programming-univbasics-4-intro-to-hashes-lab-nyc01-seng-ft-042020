382bf3075e22701b3abc37f0aa7cd28bf9bdf537
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-23 14:55:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,8 @@ end
 def my_hash_creator(key, value)
   my_hash_creator = {
     :nationality => "italian"
+}
 
-  }
   # return a hash that includes the key and value parameters passed into this method.
   #In this method, create and return a hash with one key/value pair using the first paramter as the key
   #and the second paramater as the value
